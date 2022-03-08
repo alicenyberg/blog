a19a7e5ff6d79db51f3e2eaa2ef0c295628fd0b7
antmar0417 <tonyboy@live.se>
2022-03-08 14:14:06 +0100

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -14,6 +15,10 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $user = Auth::user();
+
+        return view('dashboard', [
+            'user' => $user
+        ]);
     }
 }
