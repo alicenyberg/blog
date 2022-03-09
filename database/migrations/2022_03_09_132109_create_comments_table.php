d1953e0a497898d6b31426f8de8521e6d3408756
antmar0417 <tonyboy@live.se>
2022-03-09 16:11:20 +0100

    Removed likes from comments table

--- database/migrations/2022_03_09_132109_create_comments_table.php
+++ database/migrations/2022_03_09_132109_create_comments_table.php
@@ -16,9 +16,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->timestamps();
             $table->text('content');
-            $table->integer('likes');
+            $table->timestamps();
         });
     }
 
