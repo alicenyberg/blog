f694ab777f2bf695e5c3bb1d1537e7451bda05e9
antmar0417 <tonyboy@live.se>
2022-03-14 23:03:00 +0100

    added a navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,10 +10,33 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="">Blog</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register">Sign Up</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
 
     <!-- blog post here -->
 
@@ -43,12 +66,12 @@
         </div>
         <button type="submit">Login</button>
     </form>
-    <p>No account? <a href="register">Sign up here!</p>
+    {{-- <p>No account? <a href="register">Sign up here!</p> --}}
 
     <!-- register -->
 
     <!-- <form action="/register" method="post">
-        @csrf
+        {{-- @csrf --}}
         <div>
             <label for="name">Name</label>
             <input name="name" id="name" type="text" />
@@ -63,7 +86,7 @@
         </div>
         <button type="submit">Register</button>
     </form> -->
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
