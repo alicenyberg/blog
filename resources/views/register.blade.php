e361058456f5b421af608ad33824c1f1e6476323
antmar0417 <tonyboy@live.se>
2022-03-15 12:57:19 +0100

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,16 +1,47 @@
-<form action="/register" method="post">
-    @csrf
-    <div>
-        <label for="name">Name</label>
-        <input name="name" id="name" type="text" />
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-3">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Register') }}</div>
+                    @if ($errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        <p>
+                            <u>{{ $errors->first() }}</u>
+                        </p>
+                    </div>
+                    @endif
+                    <form action="/register" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label class="form-label" for="name">Name</label>
+                            <input class="form-control" name="name" id="name" type="text" />
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="email">Email</label>
+                            <input class="form-control" name="email" id="email" type="email" />
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="password">Password</label>
+                            <input class="form-control" name="password" id="password" type="password" />
+                        </div>
+                        <button class="btn btn-primary" type="submit">Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div>
-        <label for="email">Email</label>
-        <input name="email" id="email" type="email" />
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input name="password" id="password" type="password" />
-    </div>
-    <button type="submit">Register</button>
-</form>
+</body>
+</html>
+
