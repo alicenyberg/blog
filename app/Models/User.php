35c55d539d6ef9ea5da529e520d88466ff6a8487
alicenyberg <alice.nyberg@hotmail.com>
2022-03-15 15:58:11 +0100

    lint fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,9 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -51,6 +53,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
     public function likes()
     {
         return $this->hasMany(Likes::class);
